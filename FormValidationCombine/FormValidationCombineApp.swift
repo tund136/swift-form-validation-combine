4f7eca50ad848ef6052d1974ca188eda5eafc646
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-15 10:30:26 +0700

    Create the project

---
+++ FormValidationCombine/FormValidationCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationCombineApp.swift
+//  FormValidationCombine
+//
+//  Created by Danh Tu on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
