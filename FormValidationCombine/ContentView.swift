b631e8df0552c2626e41aa39ee8f53e2055cd15b
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-15 11:22:09 +0700

    Load components in ContentView

--- FormValidationCombine/ContentView.swift
+++ FormValidationCombine/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var contentBuilder = FormContentBuilderImpl()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.flexible(minimum: 44))], spacing: 20) {
+                ForEach(contentBuilder.content) { component in
+                    switch component {
+                    case is TextFormComponent:
+                        TextFieldFormView()
+                    case is DateFormComponent:
+                        DateFormView()
+                    case is ButtonFormComponent:
+                        ButtonFormView()
+                    default:
+                        EmptyView()
+                    }
+                }
+            }
+            .padding(.top, 100)
+        }
+        .padding(.horizontal)
     }
 }
 
