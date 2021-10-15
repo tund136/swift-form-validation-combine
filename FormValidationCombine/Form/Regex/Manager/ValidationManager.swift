281753263b70ebf25db57c3130ee56ce0246b842
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-15 13:37:11 +0700

    Regex Validator

--- FormValidationCombine/Form/Regex/Manager/ValidationManager.swift
+++ FormValidationCombine/Form/Regex/Manager/ValidationManager.swift
@@ -11,3 +11,24 @@ protocol ValidationManager {
     func validate(_ val: Any) -> ValidationError?
 }
 
+// Regex Validator
+struct RegexValidationManagerImpl: ValidationManager {
+    private let items: [RegexFormItem]
+    
+    init(_ items: [RegexFormItem]) {
+        self.items = items
+    }
+    
+    func validate(_ val: Any) -> ValidationError? {
+        let val = (val as? String) ?? ""
+        for regexItem in items {
+            let regex = try? NSRegularExpression(pattern: regexItem.pattern)
+            let range = NSRange(location: 0, length: val.count)
+            if regex?.firstMatch(in: val, options: [], range: range) == nil {
+                return regexItem.error
+            }
+        }
+        
+        return nil
+    }
+}
